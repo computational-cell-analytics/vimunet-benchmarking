d7f8f180afd5755fd8ca09ba87c757f5e253038a
anwai98 <anwai.archit@gmail.com>
2024-01-13 17:14:29 +0100

    Add training experiment

---
+++ train_neurips_cellseg.py
@@ -0,0 +1,38 @@
+import os
+
+import argparse
+
+
+def preprocess_neurips_cellseg():
+    # let's check the preprocessing first
+    #     - data comes from here - https://drive.google.com/drive/folders/18QSSiABS8H3qtx8SZA6RQb3aH1nbc3iF
+    cmd = "nnUNetv2_plan_and_preprocess -d 703 --verify_dataset_integrity"
+    os.system(cmd)
+
+
+def train_umamba(args):
+    if args.for_all_encoder:
+        # train 2d "U-Mamba-Enc" model
+        cmd = "nnUNetv2_train 703 2d all -tr nnUNetTrainerUMambaEnc --c"
+    else:
+        # train 2d "U-Mamba-Bot" model
+        cmd = "nnUNetv2_train 703 2d all -tr nnUNetTrainerUMambaBot --c"
+
+    os.system(cmd)
+
+
+def main(args):
+    if args.preprocess:
+        preprocess_neurips_cellseg()
+
+    if args.train:
+        train_umamba(args)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--preprocess", action="store_true")
+    parser.add_argument("--train", action="store_true")
+    parser.add_argument("--for_all_encoder", action="store_true")
+    args = parser.parse_args()
+    main(args)
