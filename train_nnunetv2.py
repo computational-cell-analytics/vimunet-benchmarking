cb7ec92ff761ce9508665dd89a81179ede3602f0
Anwai Archit <52396323+anwai98@users.noreply.github.com>
2024-04-08 22:33:26 +0200

    Refactor scripts for training on LIVECell and CREMI (#4)
    Add scripts for refactoring the LIVECell and CREMI datasets to be able to train on nnUNetv2's setup

---
+++ train_nnunetv2.py
@@ -0,0 +1,82 @@
+import os
+import warnings
+import argparse
+
+import torch
+
+
+NNUNET_PATH = "/scratch/usr/nimanwai/experiments/nnunetv2_neurips_cellseg"  # too lazy to change the name hehe
+
+DATASET_MAPPING = {
+    "livecell": [205, "Dataset205_LIVECell"],
+    "cremi": [305, "Dataset305_CREMI"],
+    "neurips_cellseg": [703, "Dataset703_NeurIPSCell"]
+}
+
+
+def declare_paths(nnunet_path: str):
+    """To let the system known of the path variables where the respective folders exist (important for all components)
+    """
+    warnings.warn(
+        "Make sure you have created the directories mentioned in this functions (relative to the root directory)"
+    )
+
+    os.environ["nnUNet_raw"] = os.path.join(nnunet_path, "nnUNet_raw")
+    os.environ["nnUNet_preprocessed"] = os.path.join(nnunet_path, "nnUNet_preprocessed")
+    os.environ["nnUNet_results"] = os.path.join(nnunet_path, "nnUNet_results")
+
+
+def preprocess_data(dataset_id):
+    # let's check the preprocessing first
+    #     - The NeurIPS CellSeg data comes from here
+    #     - https://drive.google.com/drive/folders/18QSSiABS8H3qtx8SZA6RQb3aH1nbc3iF
+    cmd = f"nnUNetv2_plan_and_preprocess -d {dataset_id} --verify_dataset_integrity"
+    os.system(cmd)
+
+
+def train_nnunetv2(fold, dataset_name, dataset_id):
+    _have_splits = os.path.exists(
+        os.path.join(NNUNET_PATH, "nnUNet_preprocessed", dataset_name, "splits_final.json")
+    )
+    assert _have_splits, "You need to create the splits yourself."
+
+    # train 2d nnUNet
+    gpus = torch.cuda.device_count()
+    cmd = f"nnUNet_compile=T nnUNet_n_proc_DA=8 nnUNetv2_train {dataset_id} 2d {fold} -num_gpus {gpus} --c"
+    os.system(cmd)
+
+
+def predict_nnunetv2(fold, dataset_name, dataset_id):
+    input_dir = os.path.join(NNUNET_PATH, "test", dataset_name, "imagesTs")
+    assert os.path.exists(input_dir)
+
+    output_dir = os.path.join(NNUNET_PATH, "test", dataset_name, "predictionTs")
+
+    cmd = f"nnUNetv2_predict -i {input_dir} -o {output_dir} -d {dataset_id} -c 2d -f {fold}"
+    os.system(cmd)
+
+
+def main(args):
+    declare_paths(NNUNET_PATH)
+
+    dataset_id, dataset_name = DATASET_MAPPING[args.dataset]
+
+    if args.preprocess:
+        preprocess_data(dataset_id=dataset_id)
+
+    if args.train:
+        train_nnunetv2(fold=args.fold, dataset_name=dataset_name, dataset_id=dataset_id)
+
+    if args.predict:
+        predict_nnunetv2(fold=args.fold, dataset_name=dataset_name, dataset_id=dataset_id)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dataset", type=str, required=True)
+    parser.add_argument("--preprocess", action="store_true")
+    parser.add_argument("--train", action="store_true")
+    parser.add_argument("--predict", action="store_true")
+    parser.add_argument("--fold", type=str, default="0")
+    args = parser.parse_args()
+    main(args)
